72165714993ab5c62e0c6e13fb73d610316d340e
linzs-online <linzs.online@gmail.com>
2023-06-09 16:10:51 +0800

    first commit

---
+++ build/CMakeFiles/virtualBillboard.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/virtualBillboard.dir/virtual-billboard.cpp.o"
+  "CMakeFiles/virtualBillboard.dir/virtual-billboard.cpp.o.d"
+  "virtualBillboard"
+  "virtualBillboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/virtualBillboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
